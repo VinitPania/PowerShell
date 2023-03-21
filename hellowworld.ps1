4eca5a4fcb4afb11b344561379f5bfed9ab06783
VinitPania <vinit.pania@cloverinfotech.com>
2023-03-21 12:02:27 +0530

    same problem wiith kill // wala

--- hellowworld.ps1
+++ hellowworld.ps1
@@ -23,7 +23,7 @@ Write-Host "The java files are  $env:JAVA_HOME"
 
 #curl.exe {$env:BUILD_URL}stop 
 #curl.exe $env:BUILD_URL//term
-curl.exe $env:BUILD_URL//kill
+curl.exe {$env:BUILD_URL}kill
 
 [double]$value = 2
 [double]$val = 5
