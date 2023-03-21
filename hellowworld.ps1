6d5b8942d5ed8713e6b0cb0d6e9a08970c4d7d3c
VinitPania <vinit.pania@cloverinfotech.com>
2023-03-21 11:51:22 +0530

    To //term  -y

--- hellowworld.ps1
+++ hellowworld.ps1
@@ -21,8 +21,8 @@ Write-Host "You are log in into $env:COMPUTERNAME"
 Write-Host "The java files are  $env:JAVA_HOME"
 
 
-#curl.exe $env:BUILD_URL/stop
-curl.exe {$env:BUILD_URL}term
+curl.exe $env:BUILD_URL//stop -y
+#curl.exe $env:BUILD_URL//term
 
 [double]$value = 2
 [double]$val = 5
