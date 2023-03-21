7c493ef38c9340ccf1d34259bc19bc210efe472d
VinitPania <vinit.pania@cloverinfotech.com>
2023-03-21 14:22:31 +0530

    Checking if condition

--- api-calling.ps1
+++ api-calling.ps1
@@ -4,11 +4,12 @@ $response.username
 $response.password
 Write-Host "from jenkins : $NAMEUSR "
 
-# if($response.username -ne $env:USERNAME && $response.password -ne $env:PASSWORD){
-#     Write-Host "Username and password not  Authenticated "
-#     Write-Host "Aborting the pipeline "
-#     #curl.exe {$env:BUILD_URL}stop 
-#     #curl.exe $env:BUILD_URL//term
-#     curl.exe {$env:BUILD_URL}kill
-# }
+if($response.username -ne $NAMEUSR && $response.password -ne $PASWD){
+    Write-Host "Username and password not  Authenticated "
+    Write-Host "Aborting the pipeline "
+    Write-Host "INside if condition"
+    curl.exe {$env:BUILD_URL}stop 
+    #curl.exe $env:BUILD_URL//term
+    #curl.exe {$env:BUILD_URL}kill
+}
 
