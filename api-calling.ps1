6b43de3c403300b55227eb17f198d3f1a0b1a6d4
VinitPania <vinit.pania@cloverinfotech.com>
2023-03-21 15:26:25 +0530

    FINAL FIX i GUESS nope

--- api-calling.ps1
+++ api-calling.ps1
@@ -1,19 +1,21 @@
 $response =  Invoke-RestMethod -Uri https://64184ff975be53f451dbdfb3.mockapi.io/login/vi/Credentials 
-  
+# $NAMEUSR = "saitama" 
+# $PASWD  =  "s@itama" 
 $response.username
 $response.password
 
 
+
+Write-Output "Before If condition"
 if($response.username -notmatch $NAMEUSR -or $response.password -notmatch $PASWD){
     Write-Host "Username and password not  Authenticated "
     Write-Host "Aborting the pipeline "
-    Write-Host "INside if condition"
+    
     $response.username
     $response.password
     #curl.exe {$env:BUILD_URL}stop 
     #curl.exe $env:BUILD_URL//term
     #curl.exe {$env:BUILD_URL}kill
-
-
 }
 
+Write-Host "After if condition"
