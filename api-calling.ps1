681afa097ea6aad73ad80d4da80be4689621ebc5
VinitPania <vinit.pania@cloverinfotech.com>
2023-03-21 14:07:55 +0530

    Changes done in api

--- api-calling.ps1
+++ api-calling.ps1
@@ -1,12 +1,14 @@
 $response =  Invoke-RestMethod -Uri https://64184ff975be53f451dbdfb3.mockapi.io/login/vi/Credentials 
   
-#$response.username
-#$response.password
+$response.username
+$response.password
+Write-Host "from jenkins :$env:USERNAME"
 
-
-if($response.username -ne $env:username && $response.password -ne $env:password){
-    Write-Host "Not Authenticated "
-    Write-Host "Aborting the pipeline "
-    
-}
+# if($response.username -ne $env:USERNAME && $response.password -ne $env:PASSWORD){
+#     Write-Host "Username and password not  Authenticated "
+#     Write-Host "Aborting the pipeline "
+#     #curl.exe {$env:BUILD_URL}stop 
+#     #curl.exe $env:BUILD_URL//term
+#     curl.exe {$env:BUILD_URL}kill
+# }
 
