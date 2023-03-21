eaa2c1b5469ce1846e6fee37ddbdc1d87fcc146f
VinitPania <vinit.pania@cloverinfotech.com>
2023-03-21 15:35:15 +0530

    Pata nahi kab hoga

--- api-calling.ps1
+++ api-calling.ps1
@@ -1,13 +1,13 @@
 $response =  Invoke-RestMethod -Uri https://64184ff975be53f451dbdfb3.mockapi.io/login/vi/Credentials 
-# $NAMEUSR = "saitama" 
-# $PASWD  =  "s@itama" 
+#  $NAMEUSR = "saitama" 
+#  $PASWD  =  "s@itama" 
 $response.username
 $response.password
 
 
 
 Write-Output "Before If condition"
-if($response.username -notmatch {$NAMEUSR} -or $response.password -notmatch {$PASWD}){
+if($response.username -notmatch $env:NAMEUSR -or $response.password -notmatch $env:PASWD){
     Write-Host "Username and password not  Authenticated "
     Write-Host "Aborting the pipeline "
     
