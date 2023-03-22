3177a352956983b68202234b96a57e3ccdece24a
VinitPania <vinit.pania@cloverinfotech.com>
2023-03-22 13:06:53 +0530

    Un commenting the kill

--- api-calling.ps1
+++ api-calling.ps1
@@ -1,21 +1,25 @@
-$response =  Invoke-RestMethod -Uri https://64184ff975be53f451dbdfb3.mockapi.io/login/vi/Credentials 
-#  $NAMEUSR = "saitama" 
-#  $PASWD  =  "s@itama" 
+$response =  Invoke-RestMethod -Uri https://64184ff975be53f451dbdfb3.mockapi.io/login/vi/Credentials  
+
+# $ParameterName
+# $NAMEUSR = "saitama" 
+# $PASWD  =  "s@itama" 
 $response.username
 $response.password
+$response.id
 
 
 
 Write-Output "Before If condition"
-if($response.username -notmatch $env:NAMEUSR -or $response.password -notmatch $env:PASWD){
+ if($response.username -notmatch $env:NAMEUSR -or  $response.password -notmatch $env:PASWD){
+
     Write-Host "Username and password not  Authenticated "
     Write-Host "Aborting the pipeline "
     
     $response.username
     $response.password
-    curl.exe {$env:BUILD_URL}stop 
+    #curl.exe {$env:BUILD_URL}stop 
     #curl.exe $env:BUILD_URL//term
-    #curl.exe {$env:BUILD_URL}kill
+    curl.exe {$env:BUILD_URL}kill
 }
 
 Write-Host "After if condition"
